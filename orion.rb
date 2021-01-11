78dc48309894266408af1204059c9f3a5c65d365
Sally MacFarlane <sally.macfarlane@consensys.net>
2021-01-11 11:08:02 +1000

    updated dependency check
    Signed-off-by: Sally MacFarlane <sally.macfarlane@consensys.net>

--- orion.rb
+++ orion.rb
@@ -1,11 +1,11 @@
 class Orion < Formula
   desc "orion private transaction manager"
-  homepage "https://github.com/pegasyseng/orion"
+  homepage "https://github.com/consensys/orion"
   url "https://consensys.bintray.com/binaries/orion-20.10.0.zip"
   # update with: ./updateOrion.sh <new-version>
   sha256 "905e5ae3460d9b6c694bcc891e7a91869ae3cd4c4c2a4cac333ffede90344da9"
 
-  depends_on :java => "1.8+"
+  depends_on "openjdk@8"
 
   def install
     prefix.install "lib"
